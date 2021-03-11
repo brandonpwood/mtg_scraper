ce2861fff94baf27c2c07736b8a8394a9faec1ff
bpw42 <bpw42@cornell.edu>
2021-03-11 16:49:30 -0500

    Bare bones up and running

--- soup.py
+++ soup.py
@@ -1,37 +1,69 @@
 from bs4 import BeautifulSoup
 import requests
 import copy
-r = requests.get("https://www.mtggoldfish.com/metagame/standard#paper")
-# print(r.text)
-soup = BeautifulSoup(r.text, 'html.parser')
-#print(soup)
-
+import re
+import matplotlib.pyplot as plt
+import numpy as np
 
 class meta():
-    def __init__(self):
-        archs = []
+    def __init__(self, format):
+        r = requests.get("https://www.mtggoldfish.com/metagame/" + format + "#paper")
+        soup = BeautifulSoup(r.text, 'html.parser')
+        self.archs = []
+        self.find_all_archs(soup)
+
     def find_all_archs(self, soup):
         # Separate archetypes
-        found_archetypes = soup.find_all('div', class_ = 'archetype-tile')
+        found_archetypes = soup.find('div', id = "metagame-decks-container").find_all('div', class_ = 'archetype-tile')
         for found_archetype in found_archetypes:
             # Initialize new archetype
             self.archs.append(arch())
 
             # Find archetype name
+            title_box = found_archetype.find("div", class_ = "archetype-tile-title") # Pulls up titlebox
+            self.archs[-1].name = title_box.find('span').text[1:-1] # Grabs name from titlebox, inserts into new arch
 
+            # Find metagame percentage, number of decks
+            text = found_archetype.find('div', class_ = "archetype-tile-statistic metagame-percentage").text
+            self.archs[-1].per = float(re.findall("\d.*%", text)[0][0:-1])
+            self.archs[-1].num_decks = int(re.findall("\(.*\)", text)[0][1:-1])
+
+            # Find deck colors
+            color_string = found_archetype.find("span", class_ = "manacost").attrs['aria-label']
+            color_string = color_string[8:].split(" ")
+            self.archs[-1].colors = color_string
+
+            # Find
+            # print('----------')
+    def pie_meta(self, n):
+        # Create and save a pie chart of the current meta
+        counter = 0
+        y = []
+        my_labels = []
+        while( (counter < n) and counter < len(self.archs)):
+            y.append(self.archs[counter].per)
+            my_labels.append(self.archs[counter].name)
+            counter += 1
+        y = np.array(y)
+        plt.pie(y, labels = my_labels, autopct='%1.1f%%')
+        plt.savefig('test.png')
+
+    def print_to_terminal(self):
+        for i in self.archs:
+            i.print_to_terminal()
+            print('--------')
 class arch:
     def __init__(self):
         # Archetype statistics
         self.name = "" # Archetype name
-        self.colors = [] # Deck colors, probably better way to do this
         self.per = 0 # Metagame percentage
-        self.is_online = False # Boolean for deck format
+        self.num_decks = 0 # Number of decks in the metagame
+        self.colors = [] # Deck colors, probably better way to do this
         self.paper_price = 0 # Price in USD
         self.MTGO_price = 0 # Price in tix
         self.top_cards = ["","",""] # Names of top top_card
         self.link = "" # String consisting of a link to archetype on mtggoldfish
-    def print_to_discord (self):
-        pass
+
     def print_to_terminal(self):
         # Dump all attributes and accompanying values
         print(self.__dict__)
@@ -42,22 +74,4 @@ class arch:
         # Ensures most important values are non-defualt
         pass
 
-def make_archs(n):
-    pass
-# make_archs(5)
-a = arch()
-
-# b = soup.find_all('div', class_ = "archetype-tile-description-wrapper")
-# print(b[1])
-# c = soup.find_all('span', class_ = "deck-price-paper")
-# print(len(c))
-# for i in c:
-#     print(i.text)
-
-# deck_titles = soup.find_all('div', class_ = "archetype-tile-title")
-# for i in deck_titles:
-#     print('----------')
-#     print(i.find('span', class_ = "deck-price-online").text)
-
-
-print(find_all_archs(soup))
+M = meta('standard')
